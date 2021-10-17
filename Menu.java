8620ba1a9aa6988f97fbade0101c6d4d43de20fe
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2021-10-17 18:41:58 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,30 +1,26 @@
 public class Menu {
 
-	private int promo_items;
-	private int alacarte_items;
-	private String item;
-	private String description;
-	
-	
-	public void addMenuItem( String item , String description) {
-		this.item=item;
-		this.description=description;
-		throw new UnsupportedOperationException();
-	}
+    private int promo_items;
+    private int alacarte_items;
+    private String item;
+    private String description;
 
-	public void removeMenuItem() {
-		// TODO - implement Menu.removeMenuItem
-		throw new UnsupportedOperationException();
-	}
 
-	public void updateMenuItem() {
-		// TODO - implement Menu.updateMenuItem
-		throw new UnsupportedOperationException();
-	}
+    public void addMenuItem( String item , String description, int price) {
+        UI.Menu_of_restaurants.put(item,price);
+    }
 
-	public void displayMenu() {
-		// TODO - implement Menu.displayMenu
-		throw new UnsupportedOperationException();
-	}
+    public void removeMenuItem(String item) {
+        UI.Menu_of_restaurants.remove(item);
+    }
+
+    public void updateMenuItem() {
+        // TODO - implement Menu.updateMenuItem
+        throw new UnsupportedOperationException();
+    }
+
+    public void displayMenu() {
+        System.out.println(UI.Menu_of_restaurants);
+    }
 
 }
