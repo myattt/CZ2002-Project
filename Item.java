82e02ba1b651f9dc275e89d6eda8d28c452ae773
myattt <79566356+myattt@users.noreply.github.com>
2021-10-17 17:28:49 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,16 +1,5 @@
-public class Item {
 
-	private int type;
-	private int attribute;
-
-	public void operation() {
-		// TODO - implement Item.operation
-		throw new UnsupportedOperationException();
-	}
-
-	public void operation2() {
-		// TODO - implement Item.operation2
-		throw new UnsupportedOperationException();
-	}
-
+public interface Item {
+	public String getFood();
+	
 }
