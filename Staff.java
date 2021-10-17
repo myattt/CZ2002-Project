4cf86b3eccf4008f9e26e733acbdd74a8db99748
myattt <79566356+myattt@users.noreply.github.com>
2021-10-17 13:25:40 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -4,7 +4,14 @@ public class Staff {
 	private String gender;
 	private int employeeID;
 	private String jobTitle;
-
+	
+	public Staff(String name , String gender , int employeeID , String jobTitle){
+		this.name = name;
+		this.gender = gender;
+		this.employeeID = employeeID;
+		this.jobTitle = jobTitle;
+	}
+	
 	private void createOrder() {
 		// TODO - implement Staff.createOrder
 		throw new UnsupportedOperationException();
