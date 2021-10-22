840feba54b74e38e29260872b6a1c498a6928e9a
myattt <79566356+myattt@users.noreply.github.com>
2021-10-22 18:37:29 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,50 +1,41 @@
+package project;
+
 public class Table {
 
-	private int assigned;
-	private int tableId;
+	public int tableId;
+	private boolean assigned;
 	private int customerId;
-	private int capacityLimit;
-	public static final int tableNo=10;
+	//tableNo would be the max number of table in the list - will be set in the mainUI
+	//public static final int tableNo=10;
 	
-	public Table(int assigned, int tableId, int customerID, int capacityLimit)
-	{
-		this.assigned=assigned;
+	
+	public Table() {}
+	/*public Table(int tableId , int customerId) {
+		this.tableId = tableId;
+		this.customerId = customerId;
+	}*/
+	
+	public int gettableID() {
+		return this.tableId;
+	}
+	
+	public int getCustomerID() {
+		return this.customerId;
+	}
+	
+	public boolean isOccupied() {
+		return this.assigned;
+	}
+	
+	public void assign(int tableId , int customerId) {
 		this.tableId=tableId;
-		this.customerId=customerID;
-		this.capacityLimit=capacityLimit;
+		this.customerId = customerId;
+		this.assigned = true;
 	}
-	/**
-	 * 
-	 * @param tableID
-	 */
-	public void tableID(int tableID) {
-		// TODO - implement Table.tableID
-		throw new UnsupportedOperationException();
+	
+	public void unAssign() {
+		this.assigned = false;
+		this.tableId = 0;
+		this.customerId = 0;
 	}
-
-	public void getTableID() {
-		// TODO - implement Table.getTableID
-		throw new UnsupportedOperationException();
-	}
-
-	public void getCustomerID() {
-		// TODO - implement Table.getCustomerID
-		throw new UnsupportedOperationException();
-	}
-
-	public void isOccuppied() {
-		// TODO - implement Table.isOccuppied
-		throw new UnsupportedOperationException();
-	}
-
-	public void assign() {
-		// TODO - implement Table.assign
-		throw new UnsupportedOperationException();
-	}
-
-	public void unassign() {
-		// TODO - implement Table.unassign
-		throw new UnsupportedOperationException();
-	}
-
 }
