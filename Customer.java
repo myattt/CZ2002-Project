1ab2ae18929210ec969cef9f11196539e79e226e
myattt <79566356+myattt@users.noreply.github.com>
2021-10-22 22:56:21 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,44 @@
-public class Customer {
-
-	private int attribute;
-	private int attribute2;
-	private int attribute3;
-
-	public void operation() {
-		// TODO - implement Customer.operation
-		throw new UnsupportedOperationException();
-	}
-
+public class Customer { 
+	   
+	   private int cust_id; 
+	   private String cust_name; 
+	   private int tableId; 
+	   private boolean membership;
+	    
+	   public Customer() {}
+	   
+	   public Customer(int cust_id) { 
+	    this.cust_id = cust_id;  
+	    // TODO - implement Customer.operation 
+	    throw new UnsupportedOperationException(); 
+	   } 
+	    
+	    
+	   public int getCustomerID() { 
+	    return this.cust_id; 
+	   } 
+	    
+	   public String getCustomerName() { 
+	    return this.cust_name; 
+	   } 
+	    
+	   public int getTableId() { 
+	    return this.tableId; 
+	   } 
+	    
+	   public void setCustomerID(int cust_id) { 
+	    this.cust_id = cust_id; 
+	   } 
+	    
+	   public void setCustomerName(String cust_name) { 
+	    this.cust_name = cust_name; 
+	   } 
+	    
+	   public void setTableId(int tableId) { 
+	    this.tableId = tableId; 
+	   } 
+	   
+	   public void member(boolean membership) {
+	    this.membership = membership;
+	  }
 }
