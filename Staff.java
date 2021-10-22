65b03cfda3fc9c0c93fb08467f6c704bde421fd7
hhuppii <92574601+hhuppii@users.noreply.github.com>
2021-10-22 21:42:13 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 public class Staff {
 	
+	CustomerList custlist=new CustomerList();
 	public static final int tableNo=10;
 	public static int tablesoccupied=0;
 	public static int custID=1;
@@ -10,18 +11,56 @@ public class Staff {
 	private static int[] employeeID = {0,1,2};
 	private static String[] jobTitle = {"waiter", "waiter", "waitress"};
 	
-	public Staff(){
-	}
-	
-	private void createOrder() {
-		Order o = new Order();
-		Random rand = new Random();
 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ask cust details,id
+		boolean loop = true;
+		Scanner sc = new Scanner(System.in);
+		int input1, input2;
+		while (loop) {
+			System.out.println("(1) Check Customer Information");
+			System.out.println("(2) Create Order");
+			switch (sc.nextInt()) {
+				case 1:
+					System.out.println("Please key in customer's ID");
+					input1=sc.nextInt();
+					checkCustDetails(input1);
+					break;
+					
+				case 2:
+					createOrder();
+					break;
+				default:
+					System.out.println("Invalid command, please try again!");
+			}
+	}
+	public void createCustAcct()
+	{
+			
+	}
+	public void checkCustDetails(int custID)
+	{
+		boolean result=custlist.checkCustInfo();
+		if(result)
+		{
+			
+		}
+		else
+		{
+			createCustAcct();
+		}
+	}
+	private static void createOrder() {
+		Random rand = new Random();
+		Order o = new Order(1,2,3,4,5);
 		// Generate random integers in range 0 to 999
 		int rdint = rand.nextInt(3);
 		System.out.println("The staff who serves you is " + name[rdint] +", " + gender[rdint]+", "+jobTitle[rdint]);
 		System.out.println("Let's create your menu");
 		System.out.println();
 	}
+
 
 }
