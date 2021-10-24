2d01aa801ea4ad503c172c9601565314fbb5ef04
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2021-10-24 16:06:14 +0800

    Update Ala_Carte.java

--- Ala_Carte.java
+++ Ala_Carte.java
@@ -1,6 +1,15 @@
+import java.util.HashMap;
+
 public class Ala_Carte {
 
-	private int attribute;
+	public static HashMap<String, Integer> Menu_of_restaurants = new HashMap <String,  Integer>() {{put("Coca Cola",100);put("Ice cream",66);put("Pizza",123);put("Spaghetti",444); }};
+	//Name of item, price
+	public static HashMap<String, String> descriptions = new HashMap <String,  String>(){{put("Coca Cola","Cool");put("Ice cream","Cold");put("Pizza","Hot");put("Spaghetti","Delicious"); }};
+	//Name of item, description
+	public static HashMap<String, String> categories = new HashMap <String,  String>(){{put("Coca Cola","Drinks");put("Ice cream","Dessert");put("Pizza","Main Course");put("Spaghetti","Main Course"); }};
+	//Name of item, categories
+
+
 
 	public void operation() {
 		// TODO - implement Ala_Carte.operation
