4bc3678b7b606918c67103bf6967e67ef823290c
hhuppii <92574601+hhuppii@users.noreply.github.com>
2021-10-24 14:58:21 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,21 +1,21 @@
 package project;
+import java.util.ArrayList;
 
 public class Table {
 
 	public int tableId;
-	private boolean assigned;
+	private boolean assigned=false;
 	private int customerId;
 	private int table_size;
 	//tableNo would be the max number of table in the list - will be set in the mainUI
 	//public static final int tableNo=10;
 	
-	
-	public Table() {}
-	/*public Table(int tableId , int customerId) {
-		this.tableId = tableId;
-		this.customerId = customerId;
-	}*/
-	
+	public Table(int tableId, int table_size, int customerId) 
+	{
+		this.tableId=tableId;
+		this.table_size=table_size;
+		this.customerId=customerId;
+	}
 	public int gettableID() {
 		return this.tableId;
 	}
@@ -28,10 +28,8 @@ public class Table {
 		return this.assigned;
 	}
 	
-	public void assign(int tableId , int customerId , int table_size) {
-		this.tableId=tableId;
+	public void assign(int customerId) {
 		this.customerId = customerId;
-		this.table_size = table_size;
 		this.assigned = true;
 	}
 	
