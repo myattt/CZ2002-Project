858cb31e08cac6eebfe9262b65e31a5e45f238e6
hhuppii <92574601+hhuppii@users.noreply.github.com>
2021-10-31 00:21:13 +0800

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,105 +1,155 @@
-import java.util.Scanner;
-import java.util.*;
-public class UI{
 
-
-	private static void MakeOrder() {
-
-	}
-
-	private static void Booking() {
-		// TODO - implement UI.Booking
-		throw new UnsupportedOperationException();
-	}
-
-	private static void ModifyMenu(){
-		Scanner s = new Scanner(System.in);
-		System.out.println("1. Ala Carte or 2. Bundle or 3. Display whole menu");
-		int op = s.nextInt();
-		if(op==1) {
-			Ala_Carte m = new Ala_Carte();
-
-			int option;
-			do {
-				System.out.println("Please select option \n 1. Add item \n 2. Delete items \n 3. Clear Menu \n 4. Display the Menu \n 0. Quit");
-				option = s.nextInt();
-				s.nextLine();
-				if (option == 1) {
-					System.out.println("Enter Item you want to add");
-					String item = s.nextLine();
-					if (!m.checkifexist(item)) {
-						System.out.println("Enter the description for the item");
-						String description = s.nextLine();
-						System.out.println("Enter the price");
-						int price = s.nextInt();
-						s.nextLine();
-						System.out.println("Enter the category: 1. Main Course, 2. Drinks, 3. Dessert");
-						String cate = s.nextLine();
-						m.addItem(item, description, price, cate);
-
-					} else {
-						System.out.println("Already exists, try another name");
-					}
-				} else if (option == 2) {
-					m.displayItem();
-					System.out.println("Enter the item you want to delete");
-					String item_to_delete = s.nextLine();
-					m.removeItem(item_to_delete);
-				} else if (option == 3) {
-					Ala_Carte.Menu_of_restaurants.clear();
-				} else if (option == 4) {
-					m.displayItem();
-				} else option = 0;
-			} while (option != 0);
-		}else if(op == 2){
-			Promotional_Package m = new Promotional_Package();
-
-			int option;
-			do {
-				System.out.println("Please select option \n 1. Add bundle \n 2. Delete bundle \n 3. Display Menu \n 0. Quit");
-				option = s.nextInt();
-
-				if (option == 1) {
-					m.addItem();
-				} else if (option == 2) {
-					m.removeitem();
-				} else if (option == 3) {
-					m.displayItem();
-				} else option = 0;
-			} while (option != 0);
-		}
-		else{
-			Ala_Carte a = new Ala_Carte();
-			Promotional_Package p = new Promotional_Package();
-			a.displayItem();
-			p.displayItem();
-		}
-
-	}
-
-	private static void PrintReport() {
-
-	}
-
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int option;
-		do {
-			System.out.println("Please select option \n 1. Make Order \n 2. ModifyMenu \n 3. PrintReport");
-			option = s.nextInt();
-			s.nextLine();
-			if(option == 1) {
-				MakeOrder();
-			}
-
-			else if (option == 2) {
-				ModifyMenu();
-			}
-			else if (option == 3){
-				PrintReport();
-			}
-			else option=0;
-		}while(option != 0);
-	}
-
+import java.util.Scanner; 
+import java.util.*; 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+public class UI{ 
+ 
+ 
+ 
+ private static void MakeOrder() { 
+  System.out.print(false); 
+ } 
+ 
+ private static void Booking(TableList tablelist , CustomerList custlist){ 
+	 
+ if(tablelist.getNumEmptyTables()<=0 ) 
+	     System.out.println("All tables are filled.. Please try again later"); 
+ else {     
+	  Scanner sc = new Scanner(System.in); 
+	  System.out.println("Enter Customer Name:"); 
+	  String custName = sc.nextLine(); 
+	  sc.nextLine(); 
+	  System.out.println("Are you a member? Y/N"); 
+	  boolean membership = sc.nextBoolean(); 
+	  System.out.println("Customer pax?"); 
+	  int paxsize=sc.nextInt(); 
+	  System.out.println("Customer contact?"); 
+	  double contact=sc.nextDouble(); 
+	  System.out.println("Enter reservation month");
+	  int month=sc.nextInt();
+	  System.out.println("Enter reservation day");
+	  int day=sc.nextInt();
+	  System.out.println("Enter reservation hour"); 
+	  int hour=sc.nextInt();
+	  Calendar aDate;
+	  aDate = new GregorianCalendar(2021, month, day, hour, 00);
+	  int suitableTable = table_size(paxsize); 
+	  int tableId = tablelist.findSuitableTable(suitableTable); 
+	  if(tableId == -1) 
+	  {
+		  System.out.println("Please try again");
+	  }
+	  else { 
+      Customer customer= custlist.createCust(custName , (custlist.getCListSize())+1 , membership, paxsize, contact , aDate); 
+      int customerId = customer.getCustomerID(); 
+      tablelist.assignTable(tableId+1 , customerId); 
+      tablelist.showAssignedTables(); 
+      System.out.println("\n"); 
+      tablelist.showEmptyTables(); 
+      System.out.println("\n"); 
+    } 
+ }
+ } 
+  
+   private static int table_size(int paxsize) {  
+    int tableSize;  
+    switch(paxsize){  
+    case 1,2:  
+     tableSize = 2;  
+     break;  
+    case 3,4:  
+     tableSize = 4;  
+     break;  
+    case 5,6:  
+     tableSize = 6;  
+     break;  
+    case 7,8:  
+     tableSize = 8;  
+     break;  
+    case 9,10:  
+     tableSize = 10;  
+     break;  
+    default:  
+     System.out.println("Size unknown.");  
+     tableSize=-1;  
+     break;  
+    }  
+    return tableSize;  
+   } 
+ private static void RemoveReservation()
+ {
+	 
+ }
+ private static void ModifyMenu() { 
+  Ala_Carte m = new Ala_Carte(); 
+  Scanner s = new Scanner(System.in); 
+  int option; 
+  do { 
+   System.out.println("Please select option \n 1. Add item \n 2. Delete items \n 3. Clear Menu \n 4. Display the Menu"); 
+   option = s.nextInt(); 
+   s.nextLine(); 
+   if(option == 1) { 
+    System.out.println("Enter Item you want to add"); 
+    String item = s.nextLine(); 
+    if(!m.checkifexist(item)) { 
+     System.out.println("Enter the description for the item"); 
+     String description = s.nextLine(); 
+     System.out.println("Enter the price"); 
+     int price = s.nextInt(); 
+     s.nextLine(); 
+     System.out.println("Enter the category: 1. Main Course, 2. Drinks, 3. Dessert"); 
+     String cate = s.nextLine(); 
+     m.addItem(item, description, price, cate); 
+ 
+    }else{ 
+     System.out.println("Already exists, try another name"); 
+    } 
+   } 
+   else if (option == 2) { 
+    m.displayItem(); 
+    System.out.println("Enter the item you want to delete"); 
+    String item_to_delete = s.nextLine(); 
+    m.removeItem(item_to_delete); 
+   } 
+   else if (option == 3) { 
+    Ala_Carte.Menu_of_restaurants.clear(); 
+   } 
+   else if (option == 4){ 
+    m.displayItem(); 
+   } 
+   else option=0; 
+  }while(option != 0); 
+ } 
+ 
+ private static void PrintReport() { 
+  System.out.print(false); 
+ } 
+ 
+ public static void main(String[] args){ 
+  Scanner s = new Scanner(System.in); 
+  TableList tablelist= new TableList(); 
+  CustomerList custlist = new CustomerList(); 
+  int option; 
+  do { 
+   System.out.println("Please select option \n 1. Make Order \n 2. Create Reservation \n 3. Remove Reservation \n 4. ModifyMenu \n 5. PrintReport"); 
+   option = s.nextInt(); 
+   s.nextLine(); 
+   if(option == 1) { 
+    MakeOrder(); 
+   } 
+   else if (option == 2) { 
+    Booking(tablelist , custlist); 
+   } 
+   else if (option==3)
+   {
+	RemoveReservation();
+   }
+   else if (option == 3){ 
+    PrintReport(); 
+   } 
+   else option=0; 
+  }while(option != 0); 
+ } 
+ 
 }
