478989882785bf7791543f375383f1827e627e5c
myattt <79566356+myattt@users.noreply.github.com>
2021-10-31 02:12:46 +0800

    Update UI.java

--- UI.java
+++ UI.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 import java.util.*; 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-public class UI{ 
+public class MainUI{ 
  
  
  
  private static void MakeOrder() { 
- 		Scanner s = new Scanner(System.in);
+/* 		Scanner s = new Scanner(System.in);
 		Order o = new Order();
 		o.startOrder();
 		int op;
@@ -33,17 +33,17 @@ public class UI{
 		}
 		while(op  != 0);
 		
-		System.out.println("FINISH, LET's ADD THE ORDER INTO DATABASE TO PRINT REPORT");
+		System.out.println("FINISH, LET's ADD THE ORDER INTO DATABASE TO PRINT REPORT");*/
  } 
  
  private static void Booking(TableList tablelist , CustomerList custlist){ 
-	 if(tablelist.getNumEmptyTables()<=0) 
+	 System.out.println("****Booking Interface****");
+	 if(tablelist.getNumEmptyTables()<=0 ) 
 		     System.out.println("All tables are filled.. Please try again later"); 
-	 else{     
+	 else {     
 		  Scanner sc = new Scanner(System.in); 
 		  System.out.println("Enter Customer Name:"); 
-		  String custName = sc.nextLine(); 
-		  sc.nextLine(); 
+		  String custName = sc.nextLine();
 		  System.out.println("Are you a member? Y/N"); 
 		  boolean membership = sc.nextBoolean(); 
 		  System.out.println("Customer pax?"); 
@@ -67,7 +67,9 @@ public class UI{
 		  else { 
 	      Customer customer= custlist.createCust(custName , (custlist.getCListSize())+1 , membership, paxsize, contact , aDate); 
 	      int customerId = customer.getCustomerID(); 
-	      tablelist.assignTable(tableId+1 , customerId); 
+	      boolean assigned = tablelist.assignTable(tableId+1 , customerId);
+	      if(assigned)
+	    	  customer.setTableId(tableId+1);
 	      tablelist.showAssignedTables(); 
 	      System.out.println("\n"); 
 	      tablelist.showEmptyTables(); 
@@ -77,103 +79,130 @@ public class UI{
  } 
   
    private static int table_size(int paxsize) {  
-    int tableSize;  
-    switch(paxsize){  
-    case 1,2:  
-     tableSize = 2;  
-     break;  
-    case 3,4:  
-     tableSize = 4;  
-     break;  
-    case 5,6:  
-     tableSize = 6;  
-     break;  
-    case 7,8:  
-     tableSize = 8;  
-     break;  
-    case 9,10:  
-     tableSize = 10;  
-     break;  
-    default:  
-     System.out.println("Size unknown.");  
-     tableSize=-1;  
-     break;  
-    }  
-    return tableSize;  
+	    int tableSize;  
+	    switch(paxsize){  
+	    case 1,2:  
+	    	tableSize = 2;  
+	     	break;  
+	    case 3,4:  
+	    	tableSize = 4;  
+	     	break;  
+	    case 5,6:  
+	    	tableSize = 6;  
+	     	break;  
+	    case 7,8:  
+	    	tableSize = 8;  
+	     	break;  
+	    case 9,10:  
+	    	tableSize = 10;  
+	     	break;  
+	    default:  
+	    	System.out.println("Size unknown.");  
+	    	tableSize=-1;  
+	    	break;  
+	    }  
+	    return tableSize;  
    } 
- private static void RemoveReservation()
+   
+ private static void RemoveReservation(TableList tablelist , CustomerList custlist)
  {
-	 
+	 	System.out.println("Enter CustomerID");
+		Scanner sc = new Scanner(System.in);
+		int cust_id = sc.nextInt();
+		while(cust_id>custlist.getCListSize() || cust_id<0) {
+			System.out.println("Error!");
+			System.out.println("Enter CustomerID");
+			cust_id = sc.nextInt();
+		}
+		Customer customer =custlist.getCust(cust_id);
+		System.out.println("**** Double Check Customer Info****");
+		System.out.println("Customer ID	:" + customer.getCustomerID());
+		System.out.println("Customer Name	:" + customer.getCustomerName());
+		System.out.println("Table ID	:" + customer.getTableId()+"\n");
+		
+		System.out.println("Remove Reservation? True/False");
+		boolean choice = sc.nextBoolean();
+		
+		if(choice) {
+			tablelist.unAssignSeat(customer.getTableId());
+			System.out.println("Assigned seats");
+			tablelist.showAssignedTables();
+			System.out.println("Empty seats");
+			tablelist.showEmptyTables();
+			custlist.removeCust(cust_id);
+			System.out.println("\nCurrent Customer list");
+			custlist.printList();
+		}
  }
  private static void ModifyMenu() { 
-  Ala_Carte m = new Ala_Carte(); 
-  Scanner s = new Scanner(System.in); 
-  int option; 
-  do { 
-   System.out.println("Please select option \n 1. Add item \n 2. Delete items \n 3. Clear Menu \n 4. Display the Menu"); 
-   option = s.nextInt(); 
-   s.nextLine(); 
-   if(option == 1) { 
-    System.out.println("Enter Item you want to add"); 
-    String item = s.nextLine(); 
-    if(!m.checkifexist(item)) { 
-     System.out.println("Enter the description for the item"); 
-     String description = s.nextLine(); 
-     System.out.println("Enter the price"); 
-     int price = s.nextInt(); 
-     s.nextLine(); 
-     System.out.println("Enter the category: 1. Main Course, 2. Drinks, 3. Dessert"); 
-     String cate = s.nextLine(); 
-     m.addItem(item, description, price, cate); 
- 
-    }else{ 
-     System.out.println("Already exists, try another name"); 
-    } 
-   } 
-   else if (option == 2) { 
-    m.displayItem(); 
-    System.out.println("Enter the item you want to delete"); 
-    String item_to_delete = s.nextLine(); 
-    m.removeItem(item_to_delete); 
-   } 
-   else if (option == 3) { 
-    Ala_Carte.Menu_of_restaurants.clear(); 
-   } 
-   else if (option == 4){ 
-    m.displayItem(); 
-   } 
-   else option=0; 
-  }while(option != 0); 
- } 
- 
- private static void PrintReport() { 
-  System.out.print(false); 
+	  Ala_Carte m = new Ala_Carte(); 
+	  Scanner s = new Scanner(System.in); 
+	  int option; 
+	  do { 
+	   System.out.println("Please select option \n 1. Add item \n 2. Delete items \n 3. Clear Menu \n 4. Display the Menu"); 
+	   option = s.nextInt(); 
+	   s.nextLine(); 
+	   if(option == 1) { 
+	    System.out.println("Enter Item you want to add"); 
+	    String item = s.nextLine(); 
+	    if(!m.checkifexist(item)) { 
+	     System.out.println("Enter the description for the item"); 
+	     String description = s.nextLine(); 
+	     System.out.println("Enter the price"); 
+	     int price = s.nextInt(); 
+	     s.nextLine(); 
+	     System.out.println("Enter the category: 1. Main Course, 2. Drinks, 3. Dessert"); 
+	     String cate = s.nextLine(); 
+	     m.addItem(item, description, price, cate); 
+	 
+	    }else{ 
+	     System.out.println("Already exists, try another name"); 
+	    } 
+	   } 
+	   else if (option == 2) { 
+	    m.displayItem(); 
+	    System.out.println("Enter the item you want to delete"); 
+	    String item_to_delete = s.nextLine(); 
+	    m.removeItem(item_to_delete); 
+	   } 
+	   else if (option == 3) { 
+	    Ala_Carte.Menu_of_restaurants.clear(); 
+	   } 
+	   else if (option == 4){ 
+	    m.displayItem(); 
+	   } 
+	   else option=0; 
+	  }while(option != 0); 
+	 } 
+	 
+	 private static void PrintReport() { 
+	  System.out.print(false); 
  } 
  
  public static void main(String[] args){ 
-  Scanner s = new Scanner(System.in); 
-  TableList tablelist= new TableList(); 
-  CustomerList custlist = new CustomerList(); 
-  int option; 
-  do { 
-   System.out.println("Please select option \n 1. Make Order \n 2. Create Reservation \n 3. Remove Reservation \n 4. ModifyMenu \n 5. PrintReport"); 
-   option = s.nextInt(); 
-   s.nextLine(); 
-   if(option == 1) { 
-    MakeOrder(); 
-   } 
-   else if (option == 2) { 
-    Booking(tablelist , custlist); 
-   } 
-   else if (option==3)
-   {
-	RemoveReservation();
-   }
-   else if (option == 3){ 
-    PrintReport(); 
-   } 
-   else option=0; 
-  }while(option != 0); 
- } 
+	  Scanner s = new Scanner(System.in); 
+	  TableList tablelist= new TableList(); 
+	  CustomerList custlist = new CustomerList(); 
+	  int option; 
+	  do { 
+	   System.out.println("Please select option \n 1. Make Order \n 2. Create Reservation \n 3. Remove Reservation \n 4. ModifyMenu \n 5. PrintReport"); 
+	   option = s.nextInt(); 
+	   s.nextLine(); 
+	   if(option == 1) { 
+	    MakeOrder(); 
+	   } 
+	   else if (option == 2) { 
+	    Booking(tablelist , custlist); 
+	   } 
+	   else if (option==3)
+	   {
+		RemoveReservation(tablelist , custlist);
+	   }
+	   else if (option == 3){ 
+	    PrintReport(); 
+	   } 
+	   else option=0; 
+	  }while(option != 0); 
+	 } 
  
 }
