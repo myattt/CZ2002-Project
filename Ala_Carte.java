4395c719526a2546f35e8afa7bf852fd30c110e3
hhuppii <92574601+hhuppii@users.noreply.github.com>
2021-11-10 01:30:49 +0800

    Update Ala_Carte.java

--- Ala_Carte.java
+++ Ala_Carte.java
@@ -3,9 +3,9 @@ import java.util.HashMap;
 
 public class Ala_Carte implements Item{
 
-	public static HashMap<String, Integer> Menu_of_restaurants = new HashMap <String,  Integer>() {{put("Coca Cola",100);put("Ice cream",66);put("Pizza",123);put("Spaghetti",444); }};
+	public static HashMap<String, Integer> Menu_of_restaurants = new HashMap <String,  Integer>() {{put("Coca Cola",5);put("Ice cream",2);put("Pizza",8);put("Spaghetti",8); }};
 	//Name of item, price
-	public static HashMap<String, String> descriptions = new HashMap <String,  String>(){{put("Coca Cola","Cool");put("Ice cream","Cold");put("Pizza","Hot");put("Spaghetti","Delicious"); }};
+	public static HashMap<String, String> descriptions = new HashMap <String,  String>(){{put("Coca Cola","Chilled drink");put("Ice cream","Vanilla-flavoured");put("Pizza","Chicken ham and pepperoni cheese toppings");put("Spaghetti","Pork meatballs with tomato sauce"); }};
 	//Name of item, description
 	public static HashMap<String, String> categories = new HashMap <String,  String>(){{put("Coca Cola","Drinks");put("Ice cream","Dessert");put("Pizza","Main Course");put("Spaghetti","Main Course"); }};
 	//Name of item, categories
