d5e8c04c495a6f2cc9706ce250aaa55c2eb65e9b
myattt <79566356+myattt@users.noreply.github.com>
2021-11-09 20:53:39 +0800

    Update CustomerList.java

--- CustomerList.java
+++ CustomerList.java
@@ -8,6 +8,11 @@ public class CustomerList {
 	static ArrayList<Customer> list = new ArrayList<Customer>();
 	int cust_id;
 	
+	/**
+	 * To print out details of all customers in the CustomerList , prints customer's Name , Id , TableId, 
+	 * Ala carte and Bundle order , Staff information and Total Price
+	 */
+	
 	public void printList() {
 		for(int i=0 ; i<list.size() ; i++) {
 			System.out.println("Customer Name:	" + list.get(i).getCustomerName());
@@ -28,6 +33,10 @@ public class CustomerList {
 		}
 	}
 	
+	/**
+	 * finds the relevant Customer object and remove it from CustomerList
+	 * @param cust_id
+	 */
 	
 	public void removeCust(int cust_id) {
 		for(int i=0; i<list.size();i++) {
@@ -38,6 +47,12 @@ public class CustomerList {
   			}
   		}
 	}
+	/**
+	 * Traverse the CustomerList to find if customer exits, by matching the cust_id from the parameter 
+	 * and stored Customer Id
+	 * @param cust_id
+	 * @return
+	 */
 	
   	public boolean checkCustInfo(int cust_id)
   	{
@@ -49,6 +64,16 @@ public class CustomerList {
   		return false;
   	}
   	
+  	/**
+  	 * Creates a new Customer object with relevant customer details in parameter, adds Customer into the CustomerList 
+  	 * and returns the Customer
+  	 * @param name
+  	 * @param cust_id
+  	 * @param membership
+  	 * @param paxsize
+  	 * @param contact
+  	 * @return
+  	 */
   
   	public Customer createCust(String name , int cust_id , boolean membership,int paxsize, double contact)
   	{
@@ -59,6 +84,14 @@ public class CustomerList {
   		return newCust;
   		
   	}
+  	
+  	/**
+  	 * Finds the Customer by matching cust_id from parameter and the Stored customer Id. 
+  	 * If found, the customer object will be returned.
+  	 * @param cust_id
+  	 * @return
+  	 */
+  	
   	public Customer getCust(int cust_id) 
   	{
   		int foundId;
@@ -70,11 +103,21 @@ public class CustomerList {
   		return null;
   	}
   	
+  	/**
+  	 * Generates a unique customer Id that is assigned to new customers
+  	 * @return
+  	 */
   	public int getCustID() 
   	{
   		return var++;
   	}
   	
+  	/**
+  	 * Traverse the CustomerList to find the relevant customer based on cust_id parameter. 
+  	 * If customer is found, returns the date array that contains date and time of Customer Reservation
+  	 * @param cust_id
+  	 * @return
+  	 */
   	public int[] getCustDate(int cust_id) {
   		int[] date=null;
   		for(int i=0; i<list.size();i++) {
@@ -87,6 +130,10 @@ public class CustomerList {
   		return date;
   	}
 	
+  	/**
+  	 * returns the size of CustomerList or the number of Customers in the CustomerList
+  	 * @return
+  	 */
   	public int getCListSize()
   	{
   		return list.size();
